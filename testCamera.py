ee7169867d7421f44eb3a0356174a12a5494fcf1
KADRI Adlane <adlan68@live.fr>
2019-04-25 00:58:24 +0200

    Update testCamera.py

--- testCamera.py
+++ testCamera.py
@@ -3,7 +3,8 @@ import numpy as np
 import cv2 as cv 
 
 
-url = "http://192.168.1.42:8080/shot.jpg"
+
+url = "http://10.100.20.250:8080/shot.jpg"
 detector = cv.CascadeClassifier('haarcascade_frontalface_default.xml')
 
 while True:
@@ -11,18 +12,22 @@ while True:
 	pic_array = np.array(bytearray(pic_resp.content),dtype=np.uint8)
 	img = cv.imdecode(pic_array, -1)
 
-	#define the screen resulation
+#define the screen resulation
 	img_scaled = cv.resize(img,None,fx= 0.4,fy= 0.4)
 	gray = cv.cvtColor(img_scaled, cv.COLOR_BGR2GRAY)
 	faces = detector.detectMultiScale(gray, scaleFactor=1.5, minNeighbors=5)
+
 	for (x,y,z,w) in faces:
 		print(x,y,z,w)
 		pic_gray = gray [y:y+w , x:x+z]
 		pic_color = img_scaled[y:y+w, x:x+z]
 		colorRectangle = (141, 250, 205)
-		cv.rectangle(img_scaled, (x,y) , (x+z,y+w) ,colorRectangle, 3)  
+		cv.rectangle(img_scaled, (x,y) , (x+z,y+w) ,colorRectangle, 3)
+		
+	    
 
 	cv.imshow("camera",img_scaled)
+
 
 	if cv.waitKey(100) & 0xFF == ord('x'):
 		break
