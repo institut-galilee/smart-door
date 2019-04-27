78fa2f3042345c28428324e46898b83e3bc8471f
KADRI Adlane <adlan68@live.fr>
2019-04-27 02:39:35 +0200

    Update AWS.py

--- AWS.py
+++ AWS.py
@@ -1,16 +1,24 @@
 import boto3 
 
+#The two faces we're gonna compare
+inputFace = "8.png"
+targetFace = "9.png"
+
+#config the region -in our account id use us-east-2
 boto3.setup_default_session(region_name='us-east-2')
+
+#use The client rekognition for face comparison 
 client = boto3.client('rekognition',
          aws_access_key_id='AKIAUG6IPVRCXTEO72GB',
          aws_secret_access_key= '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')
 
-with open("8.png", "rb") as image_file:
+#picture to base64, because we gonna use local files
+with open(inputFace, "rb") as image_file:
      input_Bytes = image_file.read()
 
-with open("9.png", "rb") as image_file:
+with open(targetFace, "rb") as image_file:
      target_Bytes = image_file.read()
-
+#lunch the function of face comparison
 AWSresponse = client.compare_faces(
 	SourceImage={
 	'Bytes': input_Bytes
@@ -21,13 +29,13 @@ AWSresponse = client.compare_faces(
 )
 faceMatches_ =AWSresponse['FaceMatches']
 faceUnMatches_ = AWSresponse['UnmatchedFaces']
-#print(AWSresponse)
+
+#display result of comparison between the two faces
 if len(faceUnMatches_)==0:
 	faceInformations = faceMatches_
 	for information in faceInformations:
 		faceSimilarity = information
 		confidence = information['Face']
-		#print("Matched With {}""%"" Similarity ".format(faceSimilarity['Similarity']))
 		print("Recognized with {}""%"" confidence ".format(confidence['Confidence']))
 
 else :
@@ -35,5 +43,5 @@ else :
 	for information in faceInformations:
 		confidence = information['Confidence']
 		print("Non recognized with {}""%"" confidence ".format(confidence))
-		#print(information['Confidence'])
+		
 
