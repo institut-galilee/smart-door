aed6c4ac10a66eec210544109d1b13db5cd82f21
KADRI Adlane <adlan68@live.fr>
2019-04-29 21:33:03 +0200

    Add files via upload
    script to detect faces in an images

---
+++ detectFaces.py
@@ -0,0 +1,31 @@
+import face_recognition
+from PIL import Image, ImageDraw 
+import os
+
+#The current Path 
+currentPath = os.path.dirname(os.path.abspath(__file__))
+#comming dir contains pictures taken for people who're in front of the camera
+dirPath = os.path.join(currentPath, "comming")
+dirPathSavingFaces = os.path.join(currentPath, "toTreat")
+f = []
+for root, dirs, files in os.walk(dirPath):
+	for file in files:
+		if file.endswith("png") or file.endswith("jpg") or file.endswith("jpeg"):
+			#print(os.path.join(root,file))
+			imgPath =os.path.join(root,file)
+			image = face_recognition.load_image_file(imgPath)
+			face_locations = face_recognition.face_locations(image, model="cnn")
+
+			for face_location in face_locations:
+
+			    # Print the location of each face in this image
+			    top, right, bottom, left = face_location
+			    #print("A face is located at pixel location Top: {}, Left: {}, Bottom: {}, Right: {}".format(top, left, bottom, right))
+
+			    getFaceLocation = image[top:bottom, left:right]
+			    facePILL = Image.fromarray(getFaceLocation)
+			    #facePILL.show()
+			    index = (top*14+right*17)/21 + left*7/5 + bottom*5/10 
+			    index = int(index) 
+			    facePILL.save(f'toTreat/{index}.jpg')
+			    
