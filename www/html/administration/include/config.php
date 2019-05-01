4979aa27335acb60cc7f9bcaf90c94b5419ea2b2
aobrochta <andreobrochta@gmail.com>
2019-05-01 13:32:38 +0200

    mis à jour site web

---
+++ www/html/administration/include/config.php
@@ -0,0 +1,3 @@
+<?php
+  $folder_path = ""; //insert the folder path if the smartdoor administration is not on the root folder
+?>
--- www/html/smart_door/administration/include/config.php
+++
@@ -1,3 +0,0 @@
-<?php
-  $folder_path = "smart_door"; //insert the folder path if the smartdoor administration is not on the root folder
-?>
