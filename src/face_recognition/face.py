a0c7f069a58062118a5a82e395804307842a7a8c
KADRI Adlane <adlan68@live.fr>
2019-05-04 20:14:26 +0200

    Update face.py
    last version

--- src/face_recognition/face.py
+++ src/face_recognition/face.py
@@ -37,6 +37,7 @@ def insert(recognizedOrNOT,namePerson):
 	print("{} inserted".format(namePerson))
 	connection.commit()      
 
+
 def recognition():
 	#The current Path 
 	currentPath = os.path.dirname(os.path.abspath(__file__))
@@ -44,6 +45,7 @@ def recognition():
 	dirPath = os.path.join(currentPath, "comming")
 	dirPathSavingFaces = os.path.join(currentPath, "toTreat")
 	filelist = [ f for f in os.listdir(dirPathSavingFaces) if f.endswith("png") or f.endswith("jpg") or f.endswith("jpeg")  ]
+	detected = False 
 	for f in filelist:
 	    os.remove(os.path.join(dirPathSavingFaces, f))
 
@@ -54,12 +56,15 @@ def recognition():
 				imgPath =os.path.join(root,file)
 				image = face_recognition.load_image_file(imgPath)
 				face_locations = face_recognition.face_locations(image)
+				if len(face_locations)!=0 and detected == False:
+				    	detected = True
 				for face_location in face_locations:
 
 				    # Print the location of each face in this image
 				    top, right, bottom, left = face_location
-				    #print("A face is located at pixel location Top: {}, Left: {}, Bottom: {}, Right: {}".format(top, left, bottom, right))
+				    
 
+				    #print("A face is located at pixel location Top: {}, Left: {}, Bottom: {}, Right: {}".format(top, left, bottom, right))
 				    getFaceLocation = image[top-20:bottom+20, left-20:right+20]
 				    facePILL = Image.fromarray(getFaceLocation)
 				    #facePILL.show()
@@ -74,56 +79,62 @@ def recognition():
 
 								#""" RECOGNITION """#			    
 
-	#The current Path 
-	currentPath = os.path.dirname(os.path.abspath(__file__))
-	#connu dir contains pictures taken for known people 
-	dirPathConnu = os.path.join(currentPath, "connu")
-	dataBase = []
-	names= []
 
-	for root, dirs, files in os.walk(dirPathConnu):
-		for file in files:
-			if file.endswith("png") or file.endswith("jpg") or file.endswith("jpeg"):
-				imgPath =os.path.join(root,file)
-				imageKnown = face_recognition.load_image_file(imgPath)
-				#print(len(face_recognition.face_encodings(imageKnown)))
-				fileEncoded = face_recognition.face_encodings(imageKnown)[0]
-				dataBase.append(fileEncoded)
-				names.append(os.path.splitext(file)[0])
-				
-	dirPath = os.path.join(currentPath, "toTreat")
-	dataBase2 = []
+	if detected : 
+		#The current Path 
+		currentPath = os.path.dirname(os.path.abspath(__file__))
+		#connu dir contains pictures taken for known people 
+		dirPathConnu = os.path.join(currentPath, "connu")
+		dataBase = []
+		names= []
 
-	for root, dirs, files in os.walk(dirPath):
-		for file in files:
-			if file.endswith("png") or file.endswith("jpg") or file.endswith("jpeg"):
-				#print(os.path.join(root,file))
-				imgPath =os.path.join(root,file)
-				imageKnown = face_recognition.load_image_file(imgPath)
-				fileEncoded = face_recognition.face_encodings(imageKnown)[0]
-				dataBase2.append(fileEncoded)
+		for root, dirs, files in os.walk(dirPathConnu):
+			for file in files:
+				if file.endswith("png") or file.endswith("jpg") or file.endswith("jpeg"):
+					imgPath =os.path.join(root,file)
+					imageKnown = face_recognition.load_image_file(imgPath)
+					#print(len(face_recognition.face_encodings(imageKnown)))
+					fileEncoded = face_recognition.face_encodings(imageKnown)[0]
+					dataBase.append(fileEncoded)
+					names.append(os.path.splitext(file)[0])
+					
+		dirPath = os.path.join(currentPath, "toTreat")
+		dataBase2 = []
 
-	recognized= []
-	for pictEncoded in dataBase2: 
-		response = face_recognition.compare_faces(dataBase, pictEncoded)
-		listResult= pd.Series(response)
-		#print(s[s].index.values)
-		for val in listResult[listResult].index.values :
-			if names[val] not in recognized : 
-				recognized.append(names[val])
+		for root, dirs, files in os.walk(dirPath):
+			for file in files:
+				if file.endswith("png") or file.endswith("jpg") or file.endswith("jpeg"):
+					#print(os.path.join(root,file))
+					imgPath =os.path.join(root,file)
+					imageKnown = face_recognition.load_image_file(imgPath)
+					fileEncoded = face_recognition.face_encodings(imageKnown)[0]
+					dataBase2.append(fileEncoded)
 
-	print(recognized)
-	if(len(recognized)==0):
-		insert(0,"unknown")
-		subject = "WARNING"
-		msg = "Attention! \n il y a un inconnu devant la porte"
-	else: 
-		subject = "IFORMATION"
-		a = "these person have returned: \n"
-		b = "this person has returned: \n"
-		msg = "We could detect that %s "%(a if len(recognized)>1 else b)  
-		for personName in recognized:
-			insert(1,personName) 
-			msg = msg + "- " + personName + "\n"			
-	sms.sendingTEXT(subject, msg)
-recognition()
+		recognized= []
+		for pictEncoded in dataBase2: 
+			response = face_recognition.compare_faces(dataBase, pictEncoded)
+			listResult= pd.Series(response)
+			#print(s[s].index.values)
+			for val in listResult[listResult].index.values :
+				if names[val] not in recognized : 
+					recognized.append(names[val])
+
+		print(recognized)
+		if(len(recognized)==0):
+			insert(0,"unknown")
+			subject = "WARNING"
+			msg = "Attention! \n il y a un inconnu devant la porte"
+		else: 
+			subject = "IFORMATION"
+			a = "these person have returned: \n"
+			b = "this person has returned: \n"
+			msg = "We could detect that %s "%(a if len(recognized)>1 else b)  
+			for personName in recognized:
+				insert(1,personName) 
+				msg = msg + "- " + personName + "\n"			
+		sms.sendingTEXT(subject, msg)
+	else : 
+		print("any person detected")
+
+if __name__ == '__main__':
+	recognition()
