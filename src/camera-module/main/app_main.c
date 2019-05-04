c739112aa51259645ac4b8ed59332cd4335d3b1a
Ameni MTIBAA <ameni.mtibaa17@gmail.com>
2019-05-04 17:27:27 +0200

    "add ov2640 camera server"

---
+++ src/camera-module/main/app_main.c
@@ -0,0 +1,218 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/semphr.h"
+#include "freertos/event_groups.h"
+
+#include "esp_system.h"
+#include "esp_wifi.h"
+#include "esp_event_loop.h"
+#include "esp_log.h"
+#include "esp_err.h"
+#include "nvs_flash.h"
+
+
+#include "driver/gpio.h"
+#include "camera.h"
+#include "bitmap.h"
+#include "http_server.h"
+
+
+static void handle_jpg(http_context_t http_ctx, void* ctx);
+static void handle_jpg_stream(http_context_t http_ctx, void* ctx);
+static esp_err_t event_handler(void *ctx, system_event_t *event);
+static void initialise_wifi(void);
+
+static const char* TAG = "camera_demo";
+
+static const char* STREAM_CONTENT_TYPE =
+        "multipart/x-mixed-replace; boundary=123456789000000000000987654321";
+
+static const char* STREAM_BOUNDARY = "--123456789000000000000987654321";
+
+static EventGroupHandle_t s_wifi_event_group;
+const int CONNECTED_BIT = BIT0;
+static ip4_addr_t s_ip_addr;
+static camera_pixelformat_t s_pixel_format;
+#define CAMERA_PIXEL_FORMAT CAMERA_PF_GRAYSCALE
+#define CAMERA_FRAME_SIZE CAMERA_FS_QVGA
+
+
+void app_main()
+{
+    esp_log_level_set("wifi", ESP_LOG_WARN);
+    esp_log_level_set("gpio", ESP_LOG_WARN);
+
+    esp_err_t err = nvs_flash_init();
+    if (err != ESP_OK) {
+        ESP_ERROR_CHECK( nvs_flash_erase() );
+        ESP_ERROR_CHECK( nvs_flash_init() );
+    }
+
+    ESP_ERROR_CHECK(gpio_install_isr_service(0));
+
+    //configure PIN
+    camera_config_t camera_config = {
+        .ledc_channel = LEDC_CHANNEL_0,
+        .ledc_timer = LEDC_TIMER_0,
+        .pin_d0 = CONFIG_D0,
+        .pin_d1 = CONFIG_D1,
+        .pin_d2 = CONFIG_D2,
+        .pin_d3 = CONFIG_D3,
+        .pin_d4 = CONFIG_D4,
+        .pin_d5 = CONFIG_D5,
+        .pin_d6 = CONFIG_D6,
+        .pin_d7 = CONFIG_D7,
+        .pin_xclk = CONFIG_XCLK,
+        .pin_pclk = CONFIG_PCLK,
+        .pin_vsync = CONFIG_VSYNC,
+        .pin_href = CONFIG_HREF,
+        .pin_sscb_sda = CONFIG_SDA,
+        .pin_sscb_scl = CONFIG_SCL,
+        .pin_reset = CONFIG_RESET,
+        .xclk_freq_hz = CONFIG_XCLK_FREQ,
+    };
+    camera_model_t camera_model=CAMERA_OV2640;
+    //err = esp_camera_init(&camera_config);
+    err = camera_probe(&camera_config, &camera_model);
+    if (err != ESP_OK) {
+        ESP_LOGE(TAG, "Camera detection failed with error 0x%x", err);
+        return;
+    }
+     
+    //setup camera
+    ESP_LOGI(TAG, "Detected OV2640 camera, using JPEG format");
+    s_pixel_format = CAMERA_PF_JPEG;
+    camera_config.frame_size = CAMERA_FRAME_SIZE;
+    camera_config.jpeg_quality = 12; //15 normal // 12 bien // 11 ça se plante parfois ==> lower number means higher quality
+
+    camera_config.pixel_format = s_pixel_format;
+    err = camera_init(&camera_config);
+    if (err != ESP_OK) {
+        ESP_LOGE(TAG, "Camera init failed with error 0x%x", err);
+        return;
+    }
+	
+    //setup wifi
+    initialise_wifi();
+
+    //setup server
+    http_server_t server;
+    http_server_options_t http_options = HTTP_SERVER_OPTIONS_DEFAULT();    // default options: port = 80, task_affinity = tskNO_AFFINITY, 											//task_stack_size = 4096,task_priority = 1
+
+    ESP_ERROR_CHECK( http_server_start(&http_options, &server) );
+
+    if (s_pixel_format == CAMERA_PF_JPEG) {
+        ESP_ERROR_CHECK( http_register_handler(server, "/jpg", HTTP_GET, HTTP_HANDLE_RESPONSE, &handle_jpg, NULL) );
+        ESP_LOGI(TAG, "Open http://" IPSTR "/jpg for single jpg image", IP2STR(&s_ip_addr));
+        ESP_ERROR_CHECK( http_register_handler(server, "/jpg_stream", HTTP_GET, HTTP_HANDLE_RESPONSE, &handle_jpg_stream, NULL) );
+        ESP_LOGI(TAG, "Open http://" IPSTR "/jpg_stream for multipart/x-mixed-replace stream of JPEGs", IP2STR(&s_ip_addr));
+    }
+    ESP_LOGI(TAG, "Free heap: %u", xPortGetFreeHeapSize());
+    ESP_LOGI(TAG, "Camera demo ready");
+
+}
+
+static esp_err_t write_frame(http_context_t http_ctx)
+{
+    http_buffer_t fb_data = {
+            .data = camera_get_fb(),
+            .size = camera_get_data_size(),
+            .data_is_persistent = true
+    };
+    return http_response_write(http_ctx, &fb_data);
+}
+
+
+
+static void handle_jpg(http_context_t http_ctx, void* ctx)
+{   char buf[50]; int i =0;
+    esp_err_t err = camera_run();
+    if (err != ESP_OK) {
+        ESP_LOGD(TAG, "Camera capture failed with error = %d", err);
+        return;
+    }
+	
+    sprintf(buf, "image/%d.jpeg", i); i++;
+    http_response_begin(http_ctx, 200, buf, camera_get_data_size());
+    http_response_set_header(http_ctx, "Content-disposition", "inline; filename=/capture.jpg");
+    write_frame(http_ctx);
+    http_response_end(http_ctx);
+}
+
+
+
+static void handle_jpg_stream(http_context_t http_ctx, void* ctx)
+{
+    http_response_begin(http_ctx, 200, STREAM_CONTENT_TYPE, HTTP_RESPONSE_SIZE_UNKNOWN);
+
+    while (true) {
+        esp_err_t err = camera_run();
+        if (err != ESP_OK) {
+            ESP_LOGD(TAG, "Camera capture failed with error = %d", err);
+            return;
+        }
+        err = http_response_begin_multipart(http_ctx, "image/jpg",
+                camera_get_data_size());
+        if (err != ESP_OK) {
+            break;
+        }
+        err = write_frame(http_ctx);
+        if (err != ESP_OK) {
+            break;
+        }
+        err = http_response_end_multipart(http_ctx, STREAM_BOUNDARY);
+        if (err != ESP_OK) {
+            break;
+        }
+    }
+    http_response_end(http_ctx);
+}
+
+
+static esp_err_t event_handler(void *ctx, system_event_t *event)
+{
+    switch (event->event_id) {
+        case SYSTEM_EVENT_STA_START:
+            esp_wifi_connect();
+            break;
+        case SYSTEM_EVENT_STA_GOT_IP:
+            xEventGroupSetBits(s_wifi_event_group, CONNECTED_BIT);
+            s_ip_addr = event->event_info.got_ip.ip_info.ip;
+            break;
+        case SYSTEM_EVENT_STA_DISCONNECTED:
+            esp_wifi_connect();
+            xEventGroupClearBits(s_wifi_event_group, CONNECTED_BIT);
+            break;
+        default:
+            break;
+    }
+    return ESP_OK;
+}
+
+static void initialise_wifi(void)
+{
+    tcpip_adapter_init();
+    s_wifi_event_group = xEventGroupCreate();
+    ESP_ERROR_CHECK( esp_event_loop_init(event_handler, NULL) );
+    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+    ESP_ERROR_CHECK( esp_wifi_init(&cfg) );
+    ESP_ERROR_CHECK( esp_wifi_set_storage(WIFI_STORAGE_RAM) );
+    wifi_config_t wifi_config = {
+        .sta = {
+            .ssid = CONFIG_WIFI_SSID,
+            .password = CONFIG_WIFI_PASSWORD,
+        },
+    };
+    ESP_ERROR_CHECK( esp_wifi_set_mode(WIFI_MODE_STA) );
+    ESP_ERROR_CHECK( esp_wifi_set_config(WIFI_IF_STA, &wifi_config) );
+    ESP_ERROR_CHECK( esp_wifi_start() );
+    ESP_ERROR_CHECK( esp_wifi_set_ps(WIFI_PS_NONE) );
+    ESP_LOGI(TAG, "Connecting to \"%s\"", wifi_config.sta.ssid);
+    xEventGroupWaitBits(s_wifi_event_group, CONNECTED_BIT, false, true, portMAX_DELAY);
+    ESP_LOGI(TAG, "Connected");
+}
+
