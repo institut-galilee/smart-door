bd29801042150e677f7f877a94483a5d4f234789
Lucky RAHERINIAINA <raheriluc@gmail.com>
2019-05-04 16:36:15 +0200

    update web_app

--- src/web_app/smartdoor/administration/include/config.php
+++ src/web_app/smartdoor/administration/include/config.php
@@ -1,3 +1,3 @@
 <?php
-  $folder_path = ""; //insert the folder path if the smartdoor administration is not on the root folder
+  $folder_path = "smartdoor"; //insert the folder path if the smartdoor administration is not on the root folder
 ?>
