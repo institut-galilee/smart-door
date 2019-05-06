7204e849c9ebd95f6dccacd4572b911edb693f2c
KADRI Adlane <adlan68@live.fr>
2019-05-07 01:38:09 +0200

    Update detectFaces.py

--- src/open_cv/detectFaces.py
+++ src/open_cv/detectFaces.py
@@ -11,20 +11,15 @@ f = []
 for root, dirs, files in os.walk(dirPath):
 	for file in files:
 		if file.endswith("png") or file.endswith("jpg") or file.endswith("jpeg"):
-			#print(os.path.join(root,file))
 			imgPath =os.path.join(root,file)
 			image = face_recognition.load_image_file(imgPath)
 			face_locations = face_recognition.face_locations(image, model="cnn")
-
 			for face_location in face_locations:
-
 			    # Print the location of each face in this image
 			    top, right, bottom, left = face_location
-			    #print("A face is located at pixel location Top: {}, Left: {}, Bottom: {}, Right: {}".format(top, left, bottom, right))
-
 			    getFaceLocation = image[top:bottom, left:right]
 			    facePILL = Image.fromarray(getFaceLocation)
-			    #facePILL.show()
+			    #generate a number to differentiate between the images we are going to save
 			    index = (top*14+right*17)/21 + left*7/5 + bottom*5/10 
 			    index = int(index) 
 			    facePILL.save(f'toTreat/{index}.jpg')
